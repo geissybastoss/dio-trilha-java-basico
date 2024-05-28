9b3a321c0597785f35666810205ba22e65419738
geissybastoss <geissybastos@icloud.com>
2024-05-27 20:02:02 -0400

    Atualização do Codigo Conta Terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        double saldo = 237.48;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite sua agência");
+        String agencia = scanner.next();
+
+        System.out.println("Digite sua conta");
+        int conta = scanner.nextInt();
+        
+        
+        System.out.println("Olá, " + nome + ". Obrigado por criar uma conta em nosso banco!");
+        System.out.println("Sua agência é: " + agencia + ", conta: " + conta + " e seu saldo de: " + saldo + " já está disponível para saque.");
+        
     }
 }
